94391b7c1392572aa59c9506063cb4b72c4a94ce
shanecranor <shanecranor@gmail.com>
2022-08-24 16:01:08 -0600

    got assignments actually working

--- pages/index.tsx
+++ pages/index.tsx
@@ -10,22 +10,39 @@ async function fetchCourseList() {
   );
   return (await response.json());
 }
-async function fetchAssignments(course_id) {
+async function fetchAssignments(course_id: string) {
   const response = await fetch(
     `${API_URL}api/v1/courses/${course_id}/assignments?per_page=1000`
   );
   return (await response.json());
 }
-function parseCourseList(courseList) {
+function parseAssignments(assignments: any, setAssignments: any, course_id: string){
+  console.log(assignments)
+  if(!assignments[course_id]){
+    console.log(course_id)
+    return <button onClick={async () => {
+      const data = await fetchAssignments(course_id)
+      setAssignments((old) => ({...old, [course_id]: data}))
+    }}>
+      Load Assignments
+      </button>
+  }
+  return JSON.stringify(assignments[course_id])
+}
+function parseCourseList(courseList, assignments, setAssignments) {
   return courseList.map(
-    (course) => {
-      return (course?.course_code.includes("2022") && <p>{course.name}{course.id}</p>)
+    (course: any) => {
+      return (course?.course_code.includes("2022") && 
+      <>
+        <p>{course.name}{course.id}</p>
+        {parseAssignments(assignments, setAssignments, course.id)}
+      </>)
     }
   )
 }
 const Home: NextPage = () => {
   const [courseList, setCourseList] = useState();
-  const [assignments, setAssignments] = useState();
+  const [assignments, setAssignments] = useState({"40460": ["class", "youmom"]});
   return (
     <>
       <Head>
@@ -36,12 +53,12 @@ const Home: NextPage = () => {
 
       <main>
         <button onClick={async () => setCourseList(await fetchCourseList())}>get CourseList</button><br />
-        <button onClick={async () => setAssignments(await fetchAssignments("40233"))}>get Assignements</button>
+        {/* <button onClick={async () => setAssignments(await fetchAssignments("40233"))}>get Assignements</button> */}
         <br></br>
-        {courseList ? parseCourseList(courseList) : "not loaded"}
+        {courseList ? parseCourseList(courseList, assignments, setAssignments) : "not loaded"}
         <br></br>
-        {assignments && <div dangerouslySetInnerHTML={{ __html: assignments[0].description }}></div>}
-        {assignments ? JSON.stringify(assignments) : "not loaded"}
+        {/* {assignments && <div dangerouslySetInnerHTML={{ __html: assignments[0].description }}></div>}
+        {assignments ? JSON.stringify(assignments) : "not loaded"} */}
 
       </main>
     </>
