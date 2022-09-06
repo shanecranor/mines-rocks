4899719fa4935b2d7ed7d4e734ff72744c2ce773
shanecranor <70922464+shanecranor@users.noreply.github.com>
2022-09-05 21:50:48 -0600

    cleanup + works with any API key now

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,116 +1,67 @@
 import type { NextPage } from 'next'
 import Head from 'next/head'
 import Image from 'next/image'
-import { useState } from 'react'
-import Timeline, {
-  TimelineMarkers,
-  TodayMarker,
-  CursorMarker
-} from 'react-calendar-timeline'
-import moment from 'moment'
-import { redirect } from 'next/dist/server/api-utils'
-// const groups = [{ id: 1, title: 'group 1' }, { id: 2, title: 'group 2' }]
+import { useEffect, useState } from 'react'
 
-const API_URL = "https://canvas.shanecranor.workers.dev/?req="
-async function fetchCourseList() {
+import { redirect } from 'next/dist/server/api-utils'
+import Calendar from '../components/calendar/Calendar'
+let API_KEY = process.env.NEXT_PUBLIC_API_KEY
+function API_URL(key?: string) {
+  if (!API_KEY) {
+    API_KEY = localStorage.getItem('API_KEY')
+  }
+  if (key) return `https://canvas.shanecranor.workers.dev/?bearer=${key}&req=`
+  if (API_KEY) return `https://canvas.shanecranor.workers.dev/?bearer=${API_KEY}&req=`
+  console.error(".env.local API key not found and no key passed")
+}
+async function fetchCourseList(key?: string) {
   const response = await fetch(
-    `${API_URL}api/v1/courses?enrollment_state=active`
+    `${API_URL(key)}api/v1/courses?enrollment_state=active`
   );
   return (await response.json());
 }
-async function fetchAssignments(course_id: string) {
+async function fetchAssignments(course_id: string, key?: string) {
   const response = await fetch(
-    `${API_URL}api/v1/courses/${course_id}/assignments?per_page=1000`
+    `${API_URL(key)}api/v1/courses/${course_id}/assignments?per_page=1000`
   );
   return (await response.json());
 }
-function parseAssignments(assignments: any, setAssignments: any, course_id: string) {
+function parseAssignments(assignments: any, setAssignments: any, course_id: string, key?: string) {
   if (!assignments[course_id]) {
     return <button onClick={async () => {
-      const data = await fetchAssignments(course_id)
+      const data = await fetchAssignments(course_id, key)
       setAssignments((old) => ({ ...old, [course_id]: data }))
     }}>
       Load Assignments
     </button>
   }
   return <ul>{assignments[course_id].map(
-    item => <li>{item.name} {item.due_at}</li>
+    item => <li key={item.id}>{item.name} {item.due_at}</li>
   )}</ul>
 }
-function parseCourseList(courseList, assignments, setAssignments) {
+function parseCourseList(courseList: any, assignments: any, setAssignments: Function, key?: string) {
   return courseList.map(
     (course: any) => {
       return (course?.course_code.includes("2022") &&
         <>
           <p>{course.name} {course.id}</p>
-          {parseAssignments(assignments, setAssignments, course.id)}
+          {parseAssignments(assignments, setAssignments, course.id, key)}
         </>)
     }
   )
 }
 
-function assignmentsToCal(assignments, courseList, categories) {
-  const categoryList = categories.split("|")
-  const out = { groups: [], items: [] };
-  let idx = 0;
-  for (const course in assignments) {
-    const courseName = courseList.filter(item => (item.id == course))[0].course_code.split(".")[1]
-    if (assignments[course].length <= 0) { continue }
-    for (const assignment in assignments[course]) {
-      const currentAssignment = assignments[course][assignment]
-      //skip to next assignment if no due date is assigned
-      if (!currentAssignment.due_at) continue
-      let category = categoryList.filter(cat => currentAssignment.name.includes(cat))
-      if (!category.length) category.push("")
-      const groupTitle = `${courseName}: ${category[0]}`
-      const groupMatch = out.groups.filter(group => group.title == groupTitle)
-      // if no groups match the correct title create new group
-      let groupID;
-      if (!groupMatch.length) {
-        groupID = out.groups.length
-        out.groups.push({
-          id: groupID,
-          title: groupTitle,
-          groupProps: {
-            style: {
-              background: "blue"
-            }
-          }
-        })
-      } else { //otherwise get group id from match
-        groupID = groupMatch[0].id
-      }
-      console.log(currentAssignment.name)
-      console.log(currentAssignment.has_submitted_submissions)
-      const calItem = {
-        id: idx,
-        group: groupID,
-        title: currentAssignment.name,
-        start_time: moment(currentAssignment.due_at).add(-2, "day"),
-        end_time: moment(currentAssignment.due_at),
-        itemProps: {
-          style: {
-            background: currentAssignment.has_submitted_submissions ? "rgba(0,0,255,0.5)" : "red"
-          }
-        }
-      }
-      out.items.push(calItem)
-      idx++
-    }
-  }
-  console.log(out)
-  return out
-}
+
 const Home: NextPage = () => {
   const [courseList, setCourseList] = useState();
   const [assignments, setAssignments] = useState({});
-  const [calendarItems, setCalendarItems] = useState();
   const [categories, setCategories] = useState("");
+  const [canvasApiKey, setCanvasApiKey] = useState(API_KEY);
   return (
     <>
       <Head>
-        <title>Create Next App</title>
-        <meta name="description" content="Generated by create next app" />
+        <title>Canvas TODO list</title>
+        <meta name="description" content="find out what there is todo" />
         <link rel="icon" href="/favicon.ico" />
       </Head>
 
@@ -118,32 +69,30 @@ const Home: NextPage = () => {
         <p>Per Class Text Categories (use "|" as a seperator):</p>
         <input type="text"
           value={categories}
-          style={{ width: "100%", "margin-bottom": "20px" }}
+          style={{ width: "100%", "marginBottom": "20px" }}
           onChange={(e) => setCategories(e.target.value)}
         ></input>
-        <button onClick={async () => setCourseList(await fetchCourseList())}>get CourseList</button><br />
+
+        {/* render calendar */}
+        {(assignments && courseList) && <Calendar {...{ assignments, courseList, categories }} />}
+
+        <br></br>
+        <button onClick={async () => setCourseList(await fetchCourseList(canvasApiKey))}>get CourseList</button><br />
         {/* <button onClick={async () => setAssignments(await fetchAssignments("40233"))}>get Assignements</button> */}
         <br></br>
-        {courseList ? parseCourseList(courseList, assignments, setAssignments) : "not loaded"}
+        {courseList ? parseCourseList(courseList, assignments, setAssignments, canvasApiKey) : "not loaded"}
         <br></br>
         {/* {assignments && <div dangerouslySetInnerHTML={{ __html: assignments[0].description }}></div>} */}
         {/* {assignments ? JSON.stringify(assignments) : "not loaded"} */}
-        <button onClick={() => setCalendarItems(assignmentsToCal(assignments, courseList, categories))}>
-          update calendar
-        </button>
-        {calendarItems && <Timeline
-          groups={calendarItems.groups}
-          items={calendarItems.items}
-          horizontalLineClassNamesForGroup={group =>
-            [`group-${group.id}`]
-          }
-          stackItems
-          defaultTimeStart={moment().add(-2, 'day')}
-          defaultTimeEnd={moment().add(20, 'day')}
-        ><TimelineMarkers>
-            <TodayMarker />
-          </TimelineMarkers>
-        </Timeline>}
+        <p>Paste your canvas API key here:</p>
+        <input type="text"
+          value={canvasApiKey || typeof window !== 'undefined' && localStorage.getItem('API_KEY')}
+          style={{ width: "30%", "marginBottom": "20px" }}
+          onChange={(e) => {
+            setCanvasApiKey(e.target.value);
+            localStorage.setItem('API_KEY', e.target.value);
+          }}
+        ></input>
       </main>
     </>
   )
