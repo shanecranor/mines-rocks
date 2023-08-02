69cd0cf30abe44f82107af5d9df5374ab56e8080
shanecranor <shanecranor@gmail.com>
2023-08-02 17:54:47 -0500

    change description

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,12 +1,13 @@
-import './globals.css'
-import { Inter } from 'next/font/google'
-import Providers from '@/utils/provider'
-const inter = Inter({ subsets: ['latin'] })
+import "./globals.css";
+import { Inter } from "next/font/google";
+import Providers from "@/utils/provider";
+const inter = Inter({ subsets: ["latin"] });
 
 export const metadata = {
-  title: 'Canvas Aggregator',
-  description: 'View all your canvas courses in one place, see historical data, and more!',
-}
+  title: "mines.rocks: data driven course selection",
+  description:
+    "See grade distributions, assignment weights, and other historical data for your courses",
+};
 
 export default function RootLayout({
   children,
