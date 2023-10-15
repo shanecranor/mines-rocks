48c57e2dea157390e8af7a94303764bfa891d5c6
shanecranor <shanecranor@gmail.com>
2023-10-14 23:11:50 -0600

    WIP refactor cf worker

--- wrangler/cuploader/index.ts
+++ wrangler/cuploader/index.ts
@@ -1,56 +1,77 @@
 import { createClient } from '@supabase/supabase-js'
 import { RouterObj, RouteInfo, } from './types'
+import { StatusCodes } from 'http-status-codes'
 import { router } from './router'
 const API_URL = "https://elearning.mines.edu/"
-
 // eslint-disable-next-line import/no-anonymous-default-export
 export default {
   async fetch(request: Request, { SUPABASE_URL, SUPABASE_SERVICE_ROLE }: { SUPABASE_URL: string, SUPABASE_SERVICE_ROLE: string }) {
-    const supabase = createClient(SUPABASE_URL, SUPABASE_SERVICE_ROLE)
-    const { searchParams } = new URL(request.url)
-    // get the route from the url params
-    let route = <string | null>searchParams.get("route")
-    if (!route) return new Response("ERROR: No route", { status: 400 })
-    // get the canvas auth token from the url params
+    // Initialize Supabase client
+    const supabase = createClient(SUPABASE_URL, SUPABASE_SERVICE_ROLE);
 
-    //TODO: move this to the header
-    const AUTH_TOKEN = searchParams.get('bearer')
-    if (!AUTH_TOKEN) return new Response("ERROR: No auth token", { status: 401 })
+    // Get routeInfoList and Auth token from the url
+    const routeData = getRouteData(request.url);
 
-    // the router object will tell us what to do based on the route string
-    let routeInfoList = router[route]
-    if (!routeInfoList) return new Response("ERROR: Invalid route", { status: 418 })
+    // return error if routeData is invalid
+    if (routeData instanceof Response) return routeData;
 
-    // loop through each endpoint in the route
-    let status = 200
-    let responses = {}
+    // routeData is valid, extract required data
+    const { routeInfoList, AUTH_TOKEN } = routeData;
 
-    for (let i = 0; i < routeInfoList.length; i++) {
-      let routeInfo = routeInfoList[i]
-      const responseData = await doRoute(routeInfo, AUTH_TOKEN, searchParams, supabase)
-      responses = {
-        ...responses,
-        [routeInfo.endpointName]: responseData
-      }
-      if (typeof responseData === 'string') {
-        status = 500
-      }
-    }
+    // Execute canvas API calls and return the responses
+    const { responses, status } = await getResponses(routeInfoList, AUTH_TOKEN, supabase);
 
-    const encodedData = JSON.stringify(responses)
-    return new Response(
-      encodedData, {
-      headers: {
-        'Access-Control-Allow-Headers': '*',
-        'Access-Control-Allow-Origin': '*',
-        'content-type': 'application/json;charset=UTF-8'
-      },
-      status: status
-    }
-    )
+    // Build and return the final API response
+    return buildResponse(responses, status);
   },
 };
+function buildResponse(data: any, status: number) {
+  const encodedData = JSON.stringify(data)
+  return new Response(
+    encodedData, {
+    headers: {
+      //todo: restrict to mines.rocks?
+      'Access-Control-Allow-Headers': '*',
+      'Access-Control-Allow-Origin': '*',
+      'content-type': 'application/json;charset=UTF-8'
+    },
+    status: status
+  }
+  )
+}
+async function getResponses(routeInfoList: RouteInfo[], AUTH_TOKEN: string, urlParams: URLSearchParams, supabase: any) {
+  // loop through each endpoint in the route
+  let status = StatusCodes.OK
+  let responses = {}
 
+  for (let i = 0; i < routeInfoList.length; i++) {
+    let routeInfo = routeInfoList[i]
+    const responseData = await doRoute(routeInfo, AUTH_TOKEN, urlParams, supabase)
+    responses = {
+      ...responses,
+      [routeInfo.endpointName]: responseData
+    }
+    if (typeof responseData === 'string') {
+      status = StatusCodes.INTERNAL_SERVER_ERROR
+    }
+  }
+  return { responses, status }
+}
+function getRouteData(url: string) {
+  const { searchParams: urlParams } = new URL(url);
+  // get the route from the url params
+  let route = <string | null>urlParams.get("route")
+  if (!route) return new Response("ERROR: No route", { status: StatusCodes.BAD_REQUEST })
+
+  //TODO: move this to the header
+  const AUTH_TOKEN = urlParams.get('bearer')
+  if (!AUTH_TOKEN) return new Response("ERROR: No auth token", { status: StatusCodes.UNAUTHORIZED })
+
+  // the router object will tell us what to do based on the route string
+  let routeInfoList = router[route]
+  if (!routeInfoList) return new Response("ERROR: Invalid route", { status: StatusCodes.BAD_REQUEST })
+  return { routeInfoList, AUTH_TOKEN }
+}
 async function doRoute(routeInfo: RouteInfo, AUTH_TOKEN: string, searchParams: URLSearchParams, supabase: any) {
   // get the correct endpoint url
   let endpoint = routeInfo.endpoint
@@ -78,7 +99,7 @@ async function doRoute(routeInfo: RouteInfo, AUTH_TOKEN: string, searchParams: U
     }
   )
   // return the error if the response is not 200
-  if (response.status !== 200) {
+  if (response.status !== StatusCodes.OK) {
     return `ERROR: ${response.status} ${response.statusText}`
   }
   const data = await response.json()
