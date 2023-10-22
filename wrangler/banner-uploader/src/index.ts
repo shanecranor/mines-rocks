5fd00e4982865b57177432be46d9fa3f0460ed64
shanecranor <shanecranor@gmail.com>
2023-10-22 12:01:52 -0600

    fixed banner uploader

--- wrangler/banner-uploader/src/index.ts
+++ wrangler/banner-uploader/src/index.ts
@@ -1,5 +1,8 @@
 import { getCourseSearchResults, getTermCode, setTerm } from "./banner";
+import { createClient } from "@supabase/supabase-js";
 import { isSeason } from "./types";
+import { Row, cleanAndFilterData } from "../../shared-util/cleanData";
+import { isArray } from "@legendapp/state";
 
 /**
  * Welcome to Cloudflare Workers! This is your first worker.
@@ -12,55 +15,82 @@ import { isSeason } from "./types";
  */
 
 export interface Env {
-	// Example binding to KV. Learn more at https://developers.cloudflare.com/workers/runtime-apis/kv/
-	// MY_KV_NAMESPACE: KVNamespace;
-	//
-	// Example binding to Durable Object. Learn more at https://developers.cloudflare.com/workers/runtime-apis/durable-objects/
-	// MY_DURABLE_OBJECT: DurableObjectNamespace;
-	//
-	// Example binding to R2. Learn more at https://developers.cloudflare.com/workers/runtime-apis/r2/
-	// MY_BUCKET: R2Bucket;
-	//
-	// Example binding to a Service. Learn more at https://developers.cloudflare.com/workers/runtime-apis/service-bindings/
-	// MY_SERVICE: Fetcher;
+  SUPABASE_URL: string;
+  SUPABASE_SERVICE_ROLE: string;
 }
-
-
-
 
 // eslint-disable-next-line import/no-anonymous-default-export
 export default {
-	async fetch(
-		request: Request,
-		env: Env,
-		ctx: ExecutionContext
-	): Promise<Response> {
-		try {
-			const url = new URL(request.url);
-			const year = url.searchParams.get("year");
-			const season = url.searchParams.get("season")?.toLowerCase();
-			const subject = url.searchParams.get("subject");
-			const courseNumber = url.searchParams.get("courseNumber");
-			const canvasCourseID = url.searchParams.get("canvasCourseID");
-			if (!year || !season) {
-				throw new Error("Missing year or season");
-			}
-			if (!isSeason(season)) {
-				throw new Error(`Invalid season ${season}`);
-			}
-			if (subject === null || courseNumber === null) {
-				throw new Error("Missing subject or courseNumber");
-			}
-			//check if season matches the type of Season
-			const termCode = await getTermCode(season, year)
-			const { data: _, cookies } = await setTerm(termCode);
-			const courseData = await getCourseSearchResults({ termCode, subject, courseNumber }, cookies)
-			if (canvasCourseID && canvasCourseID !== null) {
-			}
-			return new Response(JSON.stringify(courseData));
-		} catch (e: any) {
-			return new Response(e.message, { status: 500 });
-		}
-		//if the data contains multiple courses, we'll upsert them all
-	},
+  async fetch(
+    request: Request,
+    env: Env,
+    ctx: ExecutionContext
+  ): Promise<Response> {
+    try {
+      const { SUPABASE_URL, SUPABASE_SERVICE_ROLE } = env;
+      const supabase = createClient(SUPABASE_URL, SUPABASE_SERVICE_ROLE);
+
+      const url = new URL(request.url);
+      const year = url.searchParams.get("year");
+      const season = url.searchParams.get("season")?.toLowerCase();
+      const subject = url.searchParams.get("subject");
+      const courseNumber = url.searchParams.get("courseNumber");
+      const canvasCourseID = url.searchParams.get("canvasCourseID");
+      if (!year || !season) {
+        throw new Error("Missing year or season");
+      }
+      if (!isSeason(season)) {
+        throw new Error(`Invalid season ${season}`);
+      }
+      if (subject === null || courseNumber === null) {
+        throw new Error("Missing subject or courseNumber");
+      }
+      //check if season matches the type of Season
+      const termCode = await getTermCode(season, year);
+      const { data: _, cookies } = await setTerm(termCode);
+      const courseData = await getCourseSearchResults(
+        { termCode, subject, courseNumber },
+        cookies
+      );
+      if (!isArray(courseData)) {
+        throw new Error(`courseData is not an array: ${courseData}`);
+      }
+      const updatedCourseData = courseData.map((row) => ({
+        ...row,
+        searchInput: `${subject}${courseNumber} ${season} ${year}`,
+      }));
+
+      //get the column names from the table
+      const { data: tableKeys } = await supabase.rpc("list_columns", {
+        table_id: "banner_course_data",
+      });
+      if (typeof courseData === "string") {
+        throw new Error(courseData);
+      }
+      //clean the data
+      const cleanedData = cleanAndFilterData(
+        updatedCourseData as Row[], //probably should add a type guard
+        tableKeys
+      );
+      //   if (canvasCourseID && canvasCourseID !== null) {
+
+      const upsertResponse = await upsertData(
+        cleanedData,
+        "banner_course_data",
+        supabase
+      );
+      // console.log(upsertResponse);
+      return new Response(JSON.stringify(cleanedData));
+    } catch (e: any) {
+      return new Response(e.message, { status: 500 });
+    }
+  },
 };
+
+export async function upsertData(data: Row[], table: string, supabase: any) {
+  // upsert the data to the supabase table
+  const upsertResponse = await supabase.from(table).upsert(data);
+  const { error } = upsertResponse;
+  if (error) return `Upsert ERROR: ${JSON.stringify(error)}`;
+  return upsertResponse;
+}
--- wrangler/cuploader/index.ts
+++ wrangler/cuploader/index.ts
@@ -1,8 +1,8 @@
-import { createClient } from '@supabase/supabase-js'
-import { Row, cleanAndFilterData } from '../shared-util/cleanData'
-import { StatusCodes } from 'http-status-codes'
-import { getResponses, getRouteInfo } from './getCanvasData'
-import { buildResponse } from './util'
+import { createClient } from "@supabase/supabase-js";
+import { Row, cleanAndFilterData } from "../shared-util/cleanData";
+import { StatusCodes } from "http-status-codes";
+import { getResponses, getRouteInfo } from "./getCanvasData";
+import { buildResponse } from "./util";
 interface Env {
   SUPABASE_URL: string;
   SUPABASE_SERVICE_ROLE: string;
@@ -14,7 +14,7 @@ export default {
     const supabase = createClient(SUPABASE_URL, SUPABASE_SERVICE_ROLE);
     // Get routeInfoList and Auth token from the url
     const routeData = getRouteInfo(request.url);
-    const { searchParams: urlParams } = new URL(request.url)
+    const { searchParams: urlParams } = new URL(request.url);
 
     // return error if routeData is invalid
     if (routeData instanceof Response) return routeData;
@@ -23,40 +23,51 @@ export default {
     const { routeInfoList, AUTH_TOKEN } = routeData;
 
     // Execute canvas API calls and return the responses
-    const { responses, status } = await getResponses(routeInfoList, AUTH_TOKEN, urlParams);
+    const { responses, status } = await getResponses(
+      routeInfoList,
+      AUTH_TOKEN,
+      urlParams
+    );
 
     //upsert data for each route in the responses
     for (const routeInfo of routeInfoList) {
       const { endpointName, supabaseTable, requiredKeys } = routeInfo;
       const data = responses[endpointName];
-      if (typeof data === 'string') {
-        return new Response(data, { status: StatusCodes.INTERNAL_SERVER_ERROR });
-      };
-      const { data: tableKeys } = await supabase.rpc(
-        'list_columns', { table_id: supabaseTable }
-      )
+      if (typeof data === "string") {
+        return new Response(data, {
+          status: StatusCodes.INTERNAL_SERVER_ERROR,
+        });
+      }
+      const { data: tableKeys } = await supabase.rpc("list_columns", {
+        table_id: supabaseTable,
+      });
       const cleanedData = cleanAndFilterData(data, tableKeys, requiredKeys);
       //update the response with the cleaned data, or an error message
-      responses[endpointName] = await upsertData(cleanedData, routeInfo.supabaseTable, supabase)
+      responses[endpointName] = await upsertData(
+        cleanedData,
+        routeInfo.supabaseTable,
+        supabase
+      );
     }
     // Build and return the final API response
     return buildResponse(responses, status);
   },
 };
 
-
-
 /**
  * Upserts data to a Supabase table.
  * @param data - The data to upsert.
  * @param table - The name of the Supabase table to upsert the data to.
  * @param supabase - The Supabase client object.
- * @returns An error message if the upsert fails 
+ * @returns An error message if the upsert fails
  */
-export async function upsertData(data: Row[], table: string, supabase: any): Promise<Row[] | string> {
+export async function upsertData(
+  data: Row[],
+  table: string,
+  supabase: any
+): Promise<Row[] | string> {
   // upsert the data to the supabase table
-  const { data: return_data, error } = await
-    supabase.from(table).upsert(data)
-  if (error) return `Upsert ERROR: ${JSON.stringify(error)}`
+  const { data: returnData, error } = await supabase.from(table).upsert(data);
+  if (error) return `Upsert ERROR: ${JSON.stringify(error)}`;
   return data;
 }
