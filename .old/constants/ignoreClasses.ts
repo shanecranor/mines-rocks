63dcc5510166e2947058e88acf4808d5dde0d8fb
shanecranor <shanecranor@gmail.com>
2023-11-02 12:04:09 -0600

    added more ignore classes

--- .old/constants/ignoreClasses.ts
+++ .old/constants/ignoreClasses.ts
@@ -1,12 +1,17 @@
 export const IGNORE_CLASSES = [
-	'Canvas API project course',
-	'CASA Advising',
-	'Sandbox - ',
-	'SANS Security Awareness Course',
-	'Tutoring',
-	'Operation Oredigger',
-	'Makerspace Training Portal',
-	'Computer Science Student Portal',
-	'Undergraduate Commencement',
-	'Graduate Commencement'
+  'Canvas API project course',
+  'CASA Advising',
+  'Sandbox - ',
+  'SANS Security Awareness Course',
+  'Tutoring',
+  'Operation Oredigger',
+  'Makerspace Training Portal',
+  'Computer Science Student Portal',
+  'Undergraduate Commencement',
+  'Graduate Commencement',
+  'Portal',
+  'Physical Metallurgy Laboratory',
+  'SAIL',
+  'BSO',
+  'Reservations'
 ]
