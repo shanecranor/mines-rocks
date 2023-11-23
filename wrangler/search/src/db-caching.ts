fdc8b21e9bedbd01f108e6a68a6e9819156c9c7b
shanecranor <shanecranor@gmail.com>
2023-11-23 11:58:38 -0700

    abstracted supabase caching layer

--- wrangler/search/src/db-caching.ts
+++ wrangler/search/src/db-caching.ts
@@ -1,30 +1,39 @@
 import { createClient } from '@supabase/supabase-js';
 import { Env } from '.';
-const classDataKey = 'https://course_summary_data.dev';
 
-export async function getCourseSummaryData(env: Env) {
-	const cache = await caches.open('supabase-course-data');
+export async function getCourseSummaryData(env: Env, ctx: ExecutionContext) {
+	return await cacheSupabaseDB('course_summary_data', env, ctx);
+}
 
-	let classDataResponse = await cache.match(classDataKey);
-	if (!classDataResponse) {
+export async function getBannerData(env: Env, ctx: ExecutionContext) {
+	return await cacheSupabaseDB('banner_course_data', env, ctx);
+}
+
+async function cacheSupabaseDB(dbName: string, env: Env, ctx: ExecutionContext) {
+	const cache = await caches.open('supabase-data');
+	const cacheKey = `https://${dbName}.dev`;
+
+	let dbDataResponse = await cache.match(cacheKey);
+	if (!dbDataResponse) {
 		const supabase = createClient(env.SUPABASE_URL, env.SUPABASE_KEY);
-		const { data, error } = await supabase.from('course_summary_data').select('*');
+		const { data, error } = await supabase.from(dbName).select('*');
 
 		if (error) {
-			await fetch(`https://feedback.mines.rocks/?site=mines-rocks&message=${encodeURI(`Supabase course fetch error: ${error.message}`)}`);
+			await fetch(
+				`https://feedback.mines.rocks/?site=mines-rocks&message=${encodeURI(`Supabase ${dbName} fetch error: ${error.message}`)}`
+			);
 			throw error;
 		}
 
 		const response = new Response(JSON.stringify(data));
-		classDataResponse = response;
+		dbDataResponse = response;
 
 		response.headers.append('Cache-Control', 's-maxage=172800');
 
-		ctx.waitUntil(cache.put(classDataKey, response.clone()));
-		await fetch('https://feedback.mines.rocks/?site=mines-rocks&message=cache_miss');
+		ctx.waitUntil(cache.put(cacheKey, response.clone()));
+		await fetch(`https://feedback.mines.rocks/?site=mines-rocks&message=cache_miss_${dbName}`);
 	} else {
 		console.log('cache hit');
 	}
-	const classData = await classDataResponse.json();
-	return classData;
+	return await dbDataResponse.json();
 }
