bdb7ba9b1d3b246d92441436e21e618bd320abff
shanecranor <shanecranor@gmail.com>
2023-11-23 18:22:36 -0700

    added more caching

--- wrangler/search/src/index.ts
+++ wrangler/search/src/index.ts
@@ -1,5 +1,6 @@
 /* eslint-disable import/no-anonymous-default-export */
 
+import { getCached } from './compute-caching';
 import { aggregateCourseData, cleanCourseData } from './data-processing/aggregation/aggregate-course-data';
 import { getBannerData, getCourseSummaryData } from './db-caching';
 
@@ -16,12 +17,13 @@ export default {
 
 		const per_page = Number(searchParams.get('per_page') || DEFAULT_PAGE_SIZE);
 		const page = Number(searchParams.get('page') || 0);
+		const search = searchParams.get('search');
 
 		const classData = (await getCourseSummaryData(env, ctx)) as any[];
 		const bannerData = (await getBannerData(env, ctx)) as any[];
 		//splice the banner data into the class data
-		// const courses = aggregateCourseData(classData, bannerData);
-		const results = classData.slice(page * per_page, (page + 1) * per_page);
+		const courses = await getCached('aggregatedCourses', () => aggregateCourseData(classData, bannerData), ctx);
+		const results = courses.slice(page * per_page, (page + 1) * per_page);
 		return new Response(JSON.stringify(results), {
 			headers: {
 				'Content-Type': 'application/json',
