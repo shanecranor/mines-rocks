6823e8762d0c8af21db1728272ee72fe7b4a36f2
shanecranor <shanecranor@gmail.com>
2023-12-20 22:40:22 -0600

    logging for canvas upload

--- wrangler/search/src/compute-caching.ts
+++ wrangler/search/src/compute-caching.ts
@@ -1,9 +1,9 @@
 import { ExecutionContext } from '@cloudflare/workers-types';
 import { log } from './logging';
 
-export async function getCached(cacheKey: string, onMiss: () => any, ctx: ExecutionContext, maxAge = 172800) {
+export async function getCached(cacheKey: string, onMiss: () => any, ctx: ExecutionContext, maxAge = 86400) {
 	const key = `https://cache.rocks/${encodeURI(cacheKey)}`;
-	const cache = await caches.open('compute-cache'); //increment to create new cache for testing
+	const cache = await caches.open('compute-cache-0'); //increment to create new cache for testing
 	let cachedResponse = await cache.match(key);
 	if (!cachedResponse) {
 		await log(`compute_cache_miss_${cacheKey}`);
